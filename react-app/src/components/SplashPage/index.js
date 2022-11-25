748e8bd02d6ab5f6760e0659fcbec4cd1e1bc389
Tan <Taan3311@gmail.com>
2022-11-25 13:29:55 -0800

    splash postimage styled

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { fetchPosts } from '../../store/posts';
 import { deleteShop, fetchShops } from '../../store/shops';
 import ShopForm from '../Shopform';
-
+import './Splash.css'
 
 function Splash(){
 const shopState = useSelector((state) => state.shops)
 console.log("hmm",shopState)
 const shops = Object.values(shopState)
+const postState = useSelector((state) => state.posts)
+const posts = Object.values(postState)
 const dispatch = useDispatch()
 useEffect(()=>{
     dispatch(fetchShops())
+    dispatch(fetchPosts())
 },[dispatch])
 
 
@@ -36,6 +40,18 @@ useEffect(()=>{
     <div>POST SHOPS</div>
     <div>
       <ShopForm/>
+    </div>
+    <div>
+      {posts.map(post =>(
+        <div className='PostImageContainer'>
+          <div className='PostImage'>
+          <img className='Image' src={post.image}></img>
+          </div>
+          <div className='PostPriceWrap'>
+          <div className='Price'>{post.price}</div>
+          </div>
+        </div>
+      ))}
     </div>
     </>
   );
