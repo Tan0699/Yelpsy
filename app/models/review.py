4b9ed9138139e4f33078bb9a1c3ed860d960e3c7
Tan <Taan3311@gmail.com>
2022-12-22 18:05:41 -0800

    review model

---
+++ app/models/review.py
@@ -0,0 +1,32 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import 
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
+    shop_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('shops.id')))
+    post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id')))
+    description = db.Column(db.String(200), nullable=False)
+    image = db.Column(db.String(2000), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default= datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default= datetime.utcnow)
+  
+  
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "description": self.description,
+            "image": self.image,
+            "rating": self.rating,
+            "shop_id": self.shop_id,
+            "post_id": self.post_id,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
