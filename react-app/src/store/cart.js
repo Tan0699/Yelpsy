828b487a8a70246109e5f88c0c17f3cae83608f4
Tan <Taan3311@gmail.com>
2022-12-27 20:58:33 -0800

    cart reducer doe

--- react-app/src/store/cart.js
+++ react-app/src/store/cart.js
@@ -1,17 +1,33 @@
 
 const ADD_TO_CART = 'posts/cart'
+const GET_ALL_CART = 'posts/seecart'
+const DELETE_FROM_CART = 'posts/delatecart'
+
+
 const addToCartAction = (payload) => {
     return {
        type: ADD_TO_CART,
        payload
    }
 }
+const getAllcartAction = (payload) => {
+    return {
+       type: GET_ALL_CART,
+       payload
+   }
+}
+const delteFromCartAction = (payload) => {
+    return {
+       type: DELETE_FROM_CART,
+       payload
+   }
+}
 
-export const addToCartThunk = product => async(dispatch) =>{
+export const addToCartThunk = (product) => async dispatch =>{
     const cart = localStorage.getItem('cart')?
      JSON.parse(localStorage.getItem('cart')): []
 
-    const dupes = cart.filter(cartItem => cartItem._id === product._id)
+    const dupes = cart.filter(cartProduct => cartProduct._id === product._id)
 
     if (dupes.length === 0){
         const addProduct = {
@@ -27,6 +43,22 @@ export const addToCartThunk = product => async(dispatch) =>{
     }
 }
 
+export const getAllcartThunk = () => async dispatch =>{
+    const cart = localStorage.getItem('cart')?
+    JSON.parse(localStorage.getItem('cart')): []
+    dispatch(getAllcartAction(cart))
+    return cart
+}
+export const deleteFromCartThunk = (product) => async dispatch =>{
+    const cart = localStorage.getItem('cart')?
+     JSON.parse(localStorage.getItem('cart')): []
+
+    const cartUpdated = cart.filter(cartProduct => cartProduct._id !== product._id)
+
+    localStorage.setItem('cart', JSON.stringify(cartUpdated))
+    dispatch(deleteFromCartThunk(cartUpdated))
+}
+
 let initialState = {
     cart:[]
 }
@@ -38,12 +70,26 @@ if (localStorage.getItem('cart')){
 }
 
 const cartReducer = (state = initialState , action) =>{
+    let newState = {}
     switch(action.type){
-        case ADD_TO_CART:
-            return {
-                cart:[...action.payload]
-            }
-            default: return state
+        case ADD_TO_CART:{
+            newState = {...state}
+            newState[cart] =[...action.payload]
+            return newState
+        }
+        case GET_ALL_CART:{
+            newState = {...state}
+            newState[cart] =[...action.payload]
+            return newState
+        }
+        case DELETE_FROM_CART:{
+            newState = {...state}
+            newState[cart] =[...action.payload]
+            return newState
+        }
+        default: {
+            return state
+        }
     }
 }
 
