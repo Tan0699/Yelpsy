cad5d134b1e849b12606da7dbd151a637f510cfe
Tan <Taan3311@gmail.com>
2022-12-28 19:44:43 -0800

    displayed quantity and whole product

--- react-app/src/components/CartPage/index.js
+++ react-app/src/components/CartPage/index.js
@@ -8,14 +8,38 @@ function Cart(){
 
     const products= useSelector((state)=> state?.cart.cart)
     console.log( products)
-
-    
-    return (
-        <>{products?.map((product)=>(
-        <div>
-           {product.name}{product.price}</div>
-        ))
+   const productsObj = {}
+    products.forEach(product =>{
+        productsObj[product.id] = (productsObj[product.id] || 0) + 1
+    })
+    const productArray = []
+    const idArry =[]
+    const filteredProducts = products.forEach(product =>{
+        if(!(idArry.includes(product.id))){
+            idArry.push(product.id)
+            productArray.push(product)
         }
+    })
+    console.log("le id filt4r",idArry)
+    console.log("le FILTEREd",productArray)
+    console.log("le",productsObj)
+    const keys = Object.keys(productsObj);
+    console.log("keyy",keys) //['12', '17', '18']
+    return (
+        <>
+        {/* {keys?.map((key)=>(
+        <div>
+            <div> {`${key}: ${productsObj[key]}`}</div>
+          <div>
+          </div>
+           </div>
+        ))
+        } */}
+        {productArray.map((product)=>(
+            <div>
+            {product.name}{product.price}quantity:{productsObj[product.id]}
+            </div>
+        ))}
         </>
     )
 }
