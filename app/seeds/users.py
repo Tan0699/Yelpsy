9f3afa00e81798d58c7ea3d0f3bd941968c9bae2
Tan <Taan3311@gmail.com>
2023-01-14 02:59:45 -0800

    new seed data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,23 +4,47 @@ from app.models import db, User, environment, SCHEMA
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        firstname='Demo', email='demo@aa.io', password='password',image="https://s3-us-west-2.amazonaws.com/assets.eastidahonews.com/wp-content/uploads/2019/04/24161406/cookie-place.jpg")
+        firstname='Demo', email='demo@aa.io', password='password',image="https://randomuser.me/api/portraits/men/93.jpg")
     marnie = User(
-        firstname='marnie', email='marnie@aa.io', password='password',image="https://images.squarespace-cdn.com/content/v1/600388b4259ef06ca93a16b2/1610844662758-AEYXMY327P8EJ70D0EB6/TCS%2B-%2BLOGO%2B2.jpg")
+        firstname='marnie', email='marnie@aa.io', password='password',image="https://randomuser.me/api/portraits/men/22.jpg")
     bobbie = User(
-        firstname='bobbie', email='bobbie@aa.io', password='password',image="https://marketplace.canva.com/EAE4zJkfVJk/1/0/1600w/canva-pink-white-minimalist-cake-shop-on-instagram-post-vYajkfQ6pbM.jpg")
+        firstname='bobbie', email='bobbie@aa.io', password='password',image="https://randomuser.me/api/portraits/men/68.jpg")
     Chen = User(
-        firstname='Chen', email='Tan@aa.io', password='password',image="https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg")
+        firstname='Chen', email='Tan@aa.io', password='password',image="https://randomuser.me/api/portraits/men/86.jpg")
     Coco = User(
-        firstname='Coco', email='Coco@aa.io', password='password',image="https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg")
+        firstname='Coco', email='Coco@aa.io', password='password',image="https://randomuser.me/api/portraits/lego/5.jpg")
     Sesi = User(
-        firstname='Sesi', email='Sesi@aa.io', password='password',image="https://glebekitchen.com/wp-content/uploads/2017/04/tonkotsuramenfront.jpg")
+        firstname='Sesi', email='Sesi@aa.io', password='password',image="https://randomuser.me/api/portraits/men/1.jpg")
+    c1 =User(firstname="Hewet",email="hsline0@mac.com",password="Layo",image="https://randomuser.me/api/portraits/women/76.jpg")
+    c2 = User(firstname="Cathy",email="creedshaw1@adobe.com",password="Longqiao",image="https://randomuser.me/api/portraits/women/2.jpg")
+    c3 = User(firstname="Carlie",email="csalthouse2@prnewswire.com",password="Bara Datu",image="https://randomuser.me/api/portraits/women/16.jpg")
+    c4 = User(firstname="Melba",email="mzanussii3@wikispaces.com",password="Rendeng",image="https://randomuser.me/api/portraits/women/68.jpg")
+    c5 = User(firstname="Stevana",email="sayliff4@histats.com",password="Okhotsk",image="https://randomuser.me/api/portraits/women/56.jpg")
+    c6 = User(firstname="Mason",email="mcollacombe5@webs.com",password="Makati City",image="https://randomuser.me/api/portraits/men/77.jpg")
+    c7 = User(firstname="Liva",email="lmasi6@thetimes.co.uk",password="Cibeureum Satu",image="https://randomuser.me/api/portraits/women/27.jpg")
+    c8 = User(firstname="Hartley",email="hnias7@psu.edu",password="São Lourenço",image="https://randomuser.me/api/portraits/men/92.jpg")
+    c9 = User(firstname="Wadsworth",email="wmaccolm8@apache.org",password="Cuispes",image="https://randomuser.me/api/portraits/men/76.jpg")
+    c10 = User(firstname="Randell",email="rducrow9@qq.com",password="Obonoma",image="https://randomuser.me/api/portraits/men/85.jpg")
+    c11 = User(firstname="Juana",email="jmcnamaraa@deviantart.com",password="Malandag",image="https://randomuser.me/api/portraits/men/12.jpg")
+    c12 = User(firstname="Lynda",email="llefleyb@wikia.com",password="Maralal",image="https://randomuser.me/api/portraits/men/45.jpg")
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
     db.session.add(Chen)
     db.session.add(Coco)
     db.session.add(Sesi)
+    db.session.add(c1)
+    db.session.add(c2)
+    db.session.add(c3)
+    db.session.add(c4)
+    db.session.add(c5)
+    db.session.add(c6)
+    db.session.add(c7)
+    db.session.add(c8)
+    db.session.add(c9)
+    db.session.add(c10)
+    db.session.add(c11)
+    
     db.session.commit()
 
 
