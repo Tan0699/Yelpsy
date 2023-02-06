70230eebe8361258b0ce1f64452d070aa66bd1a7
Tan <Taan3311@gmail.com>
2023-02-05 19:05:07 -0800

    smoge

--- app/seeds/review.py
+++ app/seeds/review.py
@@ -12,7 +12,7 @@ def seed_reviews():
     re9 = Review(created_at =  date.fromisoformat('2023-12-04'),rating=5, user_id=4, description="Very good Very good Very good Very good Very good Very good Very good Very good Very good Very good Very good Very good ",image="https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",post_id=1)
     re10 = Review(created_at =  date.fromisoformat('2023-12-04'),rating=1, user_id=4, description="this so trash i found better food digging up the trash in my neighbors yard ",image="https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",post_id=24)
     re11 = Review(created_at =  date.fromisoformat('2023-12-04'),rating=3, user_id=5, description="this is very meh for the price. I can probably make much better with a lot less money and i can't even cook",image="https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",post_id=24)
-
+    re12 = Review(created_at =  date.fromisoformat('2023-12-04'),rating=2, user_id=6, description="this took 1 month to ship for me and it was spoiled when it arrived but they did not give me a refund",image="https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",post_id=24)
     db.session.add(re1)
     db.session.add(re2)
     db.session.add(re4)
@@ -23,6 +23,8 @@ def seed_reviews():
     db.session.add(re8)
     db.session.add(re9)
     db.session.add(re10)
+    db.session.add(re11)
+    db.session.add(re12)
     db.session.commit()
 
 
